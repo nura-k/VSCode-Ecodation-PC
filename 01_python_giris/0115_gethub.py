ea7bc9f2b7efa7eabae77b23f2a447d72b51156a
VSCode-Ecodation-PC <nrkahrmn@gmail.com>
2021-03-12 04:13:35 +0300

    120321_0413

---
+++ 01_python_giris/0115_gethub.py
@@ -0,0 +1,32 @@
+#region vscode_ile_github_kullanimi
+"""
+Ad ı m  1 → "git etkin"  se ç ili  mi ?
+Yönet → Ayarlar → arama : "git etkin" → se ç ili  oldu ğ una  emin  olal ı m
+
+Ad ı m  2 → git  client  kurulu  mu ?
+Source  Control → Initialize  Repository  se ç ilemiyor  ise  kurulu  de ğ ildir . "Git"  diyerek  kural ı m .
+https : // git - scm . com.tr /
+
+Ad ı m  3 → Repo  olu ş tural ı m
+https : // github . com / → Proje  ile  ayn ı isimde  repo  olu ş tural ı m
+https : // github . com.tr / azizbektas / VSCode - Ecodation - PC . git
+
+Reklam ı m  4 → Vscode  ile  repoyu  ba ğ lamak  i ç in  "Uzaktan Ekle"
+Görünüm → Yorum  Paleti → arama : "Uzaktan Kumanda Ekle" 
+Depo  URL'si : "repo url'sini ekle"  https : // github . com.tr / azizbektas / VSCode - Ecodation - PC . git
+Uzak  İsim : "remote ismi ekle"  VSCode - Ecodation - PC
+
+Ad ı m  5 → Kimlik Doğrulama / Yetkilendirme  i ş lemi  i ç in  kal ı c ı token  olu ş tur .
+https : // github . com.tr / settings / tokens → Yeni Jeton Oluştur  
+
+Ad ı m  6 → Authenticaion / Authorization  i ş lemi  i ç in  Account  y ö ntemi  ile  ge ç ici  token  olu ş tur
+Çevirin  üzerinde  Ayarlar  Sync → çevirin  üzerinde → Sign  in → Github → Yetkili  Github → VSCode  A ç
+
+Ad ı m  8 → Tüm Değişiklikleri Aşama  
+Commit  etmeden ö nce  de ğ i ş ikliklerin ü zerinde  sa ğ t ı klay ı p  stage  yap ı yoruz
+
+Ad ı m  7 → İ lk  g ö nderimde  yada  de ğ i ş iklik  an ı nda  her  g ö nderim  i ç in  Commit  and  Push  yap ı lmal ı
+Görüntülenme  ve  Diğer  İşlemler → Kaydet  |  Yada  |  Kaynak  Kontrol → Teslim  buton → Teslim ' e  Bir İ sim  Ver  "060321-2330"
+Görünümler  ve  Diğer  İşlemler → İtme  |  Yada  |  Durum  Bar ' Kohistan'daki → Senkronize  Değişiklikler  buton , Giriş İç içinde  Jetonu  Gir 
+"""
+#endregion
