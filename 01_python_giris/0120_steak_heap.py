f3f8e0a6fe5764e3d54964a727b5f3e0cf7216d9
VSCode-Ecodation-PC <nrkahrmn@gmail.com>
2021-03-15 02:43:43 +0300

    140321-0243

--- 01_python_giris/0120_steak_heap.py
+++ 01_python_giris/0120_steak_heap.py
@@ -8,3 +8,25 @@ b = 1
 print(a)
 """
 #endregion
+
+
+#region steak_heap_aciklama
+
+#steak: ram'daki steak hafıza alanı
+"""
+→sayısal veri tipleri saklanır;
+-int
+-float
+-boolean(0,1)
+→value types diye geçer.
+→steak bölgesi adresi kopyalamaz, adres içerisindeki değeri kopyalar.
+"""
+#heap: ram'deki heap alanı
+"""
+→object veri tipleri ram'deki heap alanında saklanır.
+-list
+-object
+-str
+→referance types denir.
+"""
+#endregion
---
+++ 01_python_giris/0121_lab_pratikleri.py
@@ -0,0 +1,52 @@
+# region ornek_1
+"""
+#delta örneği
+a = 1
+b = 4
+c = 2
+delta = b**2 - 4*a*c
+print("delta değeri " , delta)
+"""
+# endregion
+
+
+# region ornek_2
+"""
+#ortalama
+s1 = 2
+s2 = 4
+ort = (s1 + s2) / 2
+print("oralama değeri ", ort)
+"""
+# endregion
+
+
+# region ornek_3
+"""
+saat bilgisini saniyeye dönüştürsün
+saat = 2
+saniye = 3600
+print("Saat: ", saat)
+print("Ekrandaki saat biriminin saniye karşılığı →", saat*saniye, " sn.")
+"""
+# endregion
+
+# ipucu → formatter shift + alt + f
+
+# region ornek_4
+"""
+# % → bölümden kalanı verir.
+# // →bölümün tam kısmını verir.
+ 
+sayi = 562
+kalan = sayi % 10
+birler = kalan // 1
+kalan = sayi % 100
+onlar = kalan // 10
+kalan = sayi % 1000
+yuzler = kalan //100
+toplamDegeri = birler + onlar + yuzler
+print(yuzler, onlar, birler)
+print(sayi, " sayısının basamakları toplamı " , toplamDegeri)
+"""
+# endregion
