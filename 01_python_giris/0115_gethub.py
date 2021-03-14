f2aa0f6593c6d6dfc898c537321110090f83dd6a
VSCode-Ecodation-PC <nrkahrmn@gmail.com>
2021-03-15 01:22:52 +0300

    150321-0122

--- 01_python_giris/0115_gethub.py
+++ 01_python_giris/0115_gethub.py
@@ -1,32 +1,32 @@
 #region vscode_ile_github_kullanimi
 """
-Ad ı m  1 → "git etkin"  se ç ili  mi ?
-Yönet → Ayarlar → arama : "git etkin" → se ç ili  oldu ğ una  emin  olal ı m
+Adım 1 → "git enable" seçili mi?
+Manage → Settings → arama: "git enable" → seçili olduğuna emin olalım
 
-Ad ı m  2 → git  client  kurulu  mu ?
-Source  Control → Initialize  Repository  se ç ilemiyor  ise  kurulu  de ğ ildir . "Git"  diyerek  kural ı m .
-https : // git - scm . com.tr /
+Adım 2 → git client kurulu mu?
+Source Control → Initialize Repository seçilemiyor ise kurulu değildir. "Install git" diyerek kuralım.
+https://git-scm.com/
 
-Ad ı m  3 → Repo  olu ş tural ı m
-https : // github . com / → Proje  ile  ayn ı isimde  repo  olu ş tural ı m
-https : // github . com.tr / azizbektas / VSCode - Ecodation - PC . git
+Adım 3 → Repo oluşturalım
+https://github.com/ → Proje ile aynı isimde repo oluşturalım
+https://github.com/azizbektas/VSCode-Ecodation-PC.git
 
-Reklam ı m  4 → Vscode  ile  repoyu  ba ğ lamak  i ç in  "Uzaktan Ekle"
-Görünüm → Yorum  Paleti → arama : "Uzaktan Kumanda Ekle" 
-Depo  URL'si : "repo url'sini ekle"  https : // github . com.tr / azizbektas / VSCode - Ecodation - PC . git
-Uzak  İsim : "remote ismi ekle"  VSCode - Ecodation - PC
+Adım 4 → Vscode ile repoyu bağlamak için "Add Remote"
+View → Comment Palette → arama: "Add Remote" 
+Repository Url: "repo url'sini ekle" https://github.com/azizbektas/VSCode-Ecodation-PC.git
+Remote Name: "remote ismi ekle" VSCode-Ecodation-PC
 
-Ad ı m  5 → Kimlik Doğrulama / Yetkilendirme  i ş lemi  i ç in  kal ı c ı token  olu ş tur .
-https : // github . com.tr / settings / tokens → Yeni Jeton Oluştur  
+Adım 5 → Authenticaion/Authorization işlemi için kalıcı token oluştur.
+https://github.com/settings/tokens → Generate New Token
 
-Ad ı m  6 → Authenticaion / Authorization  i ş lemi  i ç in  Account  y ö ntemi  ile  ge ç ici  token  olu ş tur
-Çevirin  üzerinde  Ayarlar  Sync → çevirin  üzerinde → Sign  in → Github → Yetkili  Github → VSCode  A ç
+Adım 6 → Authenticaion/Authorization işlemi için Account yöntemi ile geçici token oluştur
+Turn on Settings Sync → Turn on → Sign in → Github → Authorized Github → VSCode Aç
 
-Ad ı m  8 → Tüm Değişiklikleri Aşama  
-Commit  etmeden ö nce  de ğ i ş ikliklerin ü zerinde  sa ğ t ı klay ı p  stage  yap ı yoruz
+Adım 8 → Stage All Changes
+Commit etmeden önce değişikliklerin üzerinde sağ tıklayıp stage yapıyoruz
 
-Ad ı m  7 → İ lk  g ö nderimde  yada  de ğ i ş iklik  an ı nda  her  g ö nderim  i ç in  Commit  and  Push  yap ı lmal ı
-Görüntülenme  ve  Diğer  İşlemler → Kaydet  |  Yada  |  Kaynak  Kontrol → Teslim  buton → Teslim ' e  Bir İ sim  Ver  "060321-2330"
-Görünümler  ve  Diğer  İşlemler → İtme  |  Yada  |  Durum  Bar ' Kohistan'daki → Senkronize  Değişiklikler  buton , Giriş İç içinde  Jetonu  Gir 
+Adım 7 → İlk gönderimde yada değişiklik anında her gönderim için Commit and Push yapılmalı
+Views and More Actions → Commit | Yada | Source Control → Commit buton → Commit'e Bir İsim Ver "060321-2330"
+Views and More Actions → Push | Yada | Status Bar'daki → Synchronize Changes buton, Login İçin Token Gir 
 """
 #endregion
---
+++ 01_python_giris/0117_degiskenler.py
@@ -0,0 +1,34 @@
+#region ilk_giris
+#variable - değişkenler (değişkenler ram'de saklanır.)
+"""
+skor=12
+print(skor)
+
+name = "Ali"
+print(name)
+"""
+#endregion
+
+#region notation
+
+#camel casing (deve kasa)
+okulNumarasi = 271
+ad = "Emir"
+soyad = "Besi"
+sinavNotu = 99
+print(okulNumarasi, ad, soyad, sinavNotu)
+
+
+"""
+#undercore casing 
+okul_numarasi = 271
+ad = "Emir"
+soyad = "Besi"
+sinav_notu = 99
+print(okul_numarasi, ad, soyad, sinav_notu)
+"""
+#endregion
+
+#region degisken_isimlendirme_kurallari
+
+#endregion
