c6a5c62088337d851044fe7cbbc9e12066907ed3
VSCode-Ecodation-PC <nrkahrmn@gmail.com>
2021-03-17 00:25:44 +0300

    170321-0025

---
+++ 01_python_giris/0122_atama_operatoru.py
@@ -0,0 +1,49 @@
+# region atama_operatoru
+"""
+name = "Jhon"
+print(name)
+"""
+# endregion
+
+# region round
+# ondalıklı sayıları yuvarlamak için kullanılır.
+"""
+s = 1.838983121212
+print(round(s, 2))
+#s sayısının virgülden sonraki 2 basamağını alır.
+"""
+"""
+kg=80
+mt=1.60
+vki=kg/mt**2
+#print(vki)
+print(round(vki,2))
+"""
+# endregion
+
+# region sonucu_yine_kendi_degerine_atama
+"""
+i = 1
+print(i)
+i = i + 1
+print(i)
+sayi = 10
+sayi = sayi - 1
+sayi = sayi * 2
+x = 10
+x = x + 1
+print(sayi)
+print(x)
+"""
+# endregion
+
+# region az_sayida_degisken_kullanma
+#ne kadar az değişken kullanılırsa hafızada o kadar az yer harcanmış olur.
+"""
+sayi = 5
+toplam = 0
+toplam = toplam + sayi
+sayi = 10
+toplam = toplam + sayi
+"""
+# endregion
