3d15caf79c6ad07970280b216c04c9b42a55743b
VSCode-Ecodation-PC <nrkahrmn@gmail.com>
2021-03-24 22:32:44 +0300

    240321-2232

--- 02_karar_yapilari/0203_if.py
+++ 02_karar_yapilari/0203_if.py
@@ -1,30 +1,43 @@
-#region if
-
+#region if_aciklama
+#if kullanımı
 """
-1- Önce if yazılır
-2- koşul yazılır
-3- iki nokta ile blok başlatılır.
+1- önce if yazılır
+2- sonra koşul yazılır
+3- sonra da : iki nokta ile blok başlatılır
 """
-"""
-havaDurumu="güneşli"
-if havaDurumu == "yağmurlu" :
-    print("şemsiyeni al")
-""" 
 #endregion
 
-#region örnek 2
+#region ornek_1
 """
-a = -10
+havaDurumu = "güneşli"
+print("a")
+if havaDurumu == "yağmurlu":
+    #print("şemsiye alın lütfen")
+    print("b")
+print("c")
+"""
+#endregion
+
+#region ornek_2
+"""
+a = 10
 if a<0:
     print(f"{a} sayısı negatiftir")
 """
+#endregion
+
+#region ornek_3
 """
-a=int(input("Lütfen bir sayı giriniz : "))
-if a>0:
-    print(f"{a} sayısı pozitiftir.")
-elif a<0:
-    print(f"{a} sayısı negatiftir.") 
-else:
-    print(f"Lütfen {a} dışında bir sayı giriniz... ")
-"""    
+s = int(input("Lütfen sayı giriniz \t : "))
+if s == 0:
+    print("lütfen 0 değeri girmeyin")
+"""
+#endregion
+
+#region ornek_4
+"""
+uName = input("lütfen kullanıcı adınızı giriniz : ")
+if uName != "admin":
+    print(f"{uName} admin paneline giriş yapamaz")
+"""
 #endregion
--- 02_karar_yapilari/0204_pass.py
+++ 02_karar_yapilari/0204_pass.py
@@ -1,8 +1,12 @@
-#region
-#if bloğu boş geçilemez...
+#region pass
+
+#if bloğu boş geçilemez
+kAdi = input('Kullanici Adi Giriniz: ')
+if kAdi!= "admin":
+    pass  #yer tutucu
 """
-a=int(input("Lütfen sayı giriniz : "))
-if a==0:
-    pass
+if bloğundan sonra boş geçilemeyeceğinden dolayı yazılan ve
+öncelikle kodun başka satırları yazılıp 
+daha sonra buraya dönülmek üzere ve kodun hata vermemesi amacıyla yazılan satırdır.
 """
 #endregion
