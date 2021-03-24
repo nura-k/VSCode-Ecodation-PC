248623e775e9758aa8af1055a3ee9528354f924d
VSCode-Ecodation-PC <nrkahrmn@gmail.com>
2021-03-24 20:34:29 +0300

    240321-2034

---
+++ 02_karar_yapilari/0201_karar_verme.py
@@ -0,0 +1,18 @@
+#region karar_verme_giris
+#şu ana kadar ne yaptık? programımız
+"""
+1- aritmetik işlemler yaptı, 
+2- bize soru sorar, 
+3- verdiğimiz cevapları birleşetirdi, doğru formatta ekrana yazdır
+"""
+#endregion 
+
+#region karar_verme_asamasi
+"""
+karar vermeyi nasıl yapacak?
+KARŞILAŞTIRMA OPERATÖRLERİ İLE
+"""
+"""
+karşılaştırılacak şeyin çıktısı evet-hayır, true-false, 0-1 olmalı.
+"""
+#endregion
