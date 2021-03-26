ff2f9d4eadf4a65f8257cac75f1fdd8f42451155
VSCode-Ecodation-PC <nrkahrmn@gmail.com>
2021-03-26 22:53:33 +0300

    260321-2253

---
+++ 02_karar_yapilari/0208_elif.py
@@ -0,0 +1,40 @@
+#region elif
+"""
+havaDurumu = "serin"
+print("a")
+if havaDurumu == "yağmurlu":
+    print("şemsiye al lütfen")
+elif havaDurumu == "sıcak":
+    print("tişört giy lütfen")
+else:
+    print("kazak giy lütfen")
+    print("d")
+print("e")
+"""
+#endregion
+
+#region ornek1
+"""
+havaDurumu = "serin"
+print("a")
+if havaDurumu == "yağmurlu":
+    print("b")
+elif havaDurumu == "sıcak":
+    print("c")
+else:
+    print("d")
+print("e")
+"""
+#endregion
+
+#region ornek2
+"""
+s = int(input("l. s. g : "))
+if s>0:
+    print("pozitif")
+elif s<0:
+    print("negatif")
+else:
+    print("sıfırdır")
+"""
+#endregion
